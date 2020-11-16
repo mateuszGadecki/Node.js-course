fcda25cb1ef88a3844c88c131e84965fff7e7e8a
mateuszGadecki <mateusz.gadecki98@gmail.com>
2020-11-16 21:42:05 +0100

    Creating script which import data from file to DB

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('db connection successful');
+  });
+
+//   Read JSON file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf8')
+);
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// DELETE ALL DATA FROM COLLECTION
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
